312757e0d8c6c06a75c32327df7296542b766298
riches2020cn <141565770+riches2020cn@users.noreply.github.com>
2026-08-25 19:28:19 +0800

    Update merge_list.py

--- merge_list.py
+++ merge_list.py
@@ -8,8 +8,8 @@ SOURCES = {
     "UK": "https://iptv-org.github.io/iptv/countries/uk.m3u",
     "Australia": "https://iptv-org.github.io/iptv/countries/au.m3u",
     "Canada": "https://iptv-org.github.io/iptv/countries/ca.m3u",
-    "New Zealand": "https://iptv-org.github.io/iptv/countries/nz.m3u",
-    "Ireland": "https://iptv-org.github.io/iptv/countries/ie.m3u"
+    "New Zealand": "https://iptv-org.github.io/iptv/countries/nz.m3u",
+    "Ireland": "https://iptv-org.github.io/iptv/countries/ie.m3u"
 }
 
 OUTPUT_FILE = "IPTV_Channels.m3u"
